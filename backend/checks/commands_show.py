5f5f22bf9b01ba236c9717bf4d268a4a51b2ce67
SergeyVolkoff <vol7745@yandex.ru>
2024-07-29 09:43:00 +0300

    add in gitlab

---
+++ backend/checks/commands_show.py
@@ -0,0 +1,38 @@
+import allure
+import re
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0],'..'))
+
+from br100.model_br100 import ConnectBR
+
+br100 = ConnectBR()
+br100.check_connection
+
+
+def check_ver_platform():
+    print("Test 1", "Проверка платформы:", sep='\n')
+    try:
+        with allure.step('Отправка команды на просмотр версии платформы.'):
+            temp = br100.ssh.send_command('show version')
+        temp1 =  re.search(r'EOS\s(?P<ver_OS>\S+) (?P<ver_HW>\S+)',temp)
+        ver_HW=temp1.group('ver_HW')
+        
+        with allure.step('Сверка версии платформы - ожидается "BR-100-24f6x".'):
+            if "BR-100-24f6x" in ver_HW:
+                print(f'Platform is {ver_HW}, its - ok! ')
+                print("")
+                with open("../valueReportTest.txt", 'a') as file:
+                    file.write('Test_name:Проверка работы функционала show'+'\n'+f'ver_platform:{ver_HW}'+'\n')
+                return True
+            else:
+                print(f"Version platform wrong - {ver_HW}! ")
+                return False
+    except ValueError as err:
+        return False
+    
+def check_execute_show(command: str, exp_output: str, log_string = "", timeout: int = 5):
+    pass
+    
+if __name__ == "__main__":
+    print(check_ver_platform())
--- backend/gns/connect/model_gns.py
+++ backend/gns/connect/model_gns.py
@@ -21,10 +21,10 @@ class ConnectGNS():
         self.server_url = "http://10.27.193.245:3080"
         self.connector = Gns3Connector(url=self.server_url)
         self.list_labs = (tabulate(self.connector.projects_summary(is_print=False), headers=["Project Name"]))
-        print(self.list_labs)
+        # print(self.list_labs)
         self.name_lab = name_lab
 
-    def all_proj (self):
+    def get_all_proj (self):
 
         """ Возвращает перечень всех лаб в ГНС"""
 
@@ -40,11 +40,11 @@ class ConnectGNS():
         lab = Project(name=self.name_lab , connector=self.connector )
         lab.get()
         lab.open() # open lab
-        lab.start_nodes(poll_wait_time=5)
         CONSOLE.print(f"*** ALL nodes in {lab.name} lab ***",style='success')
         return  lab.nodes_summary()
+        
     
-    def start_nodes_from_project(self):
+    def start_all_nodes_project(self):
         
         """Старт всех устройств в лабе"""
 
@@ -55,15 +55,75 @@ class ConnectGNS():
         CONSOLE.print(f"*** ALL nodes in {lab.name} lab ***",style='success')
         return  lab.nodes_summary()
     
+    def get_lab_status(self):
+
+        """ Вернет lab_id, статус моей лабы """
+
+        lab = Project(name=self.name_lab , connector=self.connector )
+        lab.get()
+        #lab.close()
+        print(lab.project_id)
+        return f"GNS3 lab_name: {lab.name}, lab_id:{lab.project_id}, lab status: {lab.status}"
+    
+    def start_node_name(self, node_name):
+
+        """ Запуск 2go узла в проекте"""
+        
+        lab = Project(name=self.name_lab , connector=self.connector )
+        lab.get()
+        lab.open() # open lab
+        r2 = Node(
+            project_id=lab.project_id, 
+            name=node_name,
+            connector=self.connector
+            ) # создаем экз-р устр-ва
+        
+        r2.get()
+        r2.start()
+        CONSOLE.print (f'Node {r2.name} {r2.status}',style='success')
+    
+    def stop_node_name(self,node_name):
+        """ Запуск 2go узла в проекте"""
+        
+        lab = Project(name=self.name_lab , connector=self.connector )
+        lab.get()
+        lab.open() # open lab
+        r1 = Node(
+            project_id=lab.project_id, 
+            name=node_name,
+            connector=self.connector
+            ) # создаем экз-р устр-ва
+        r1.get()
+        r1.stop()
+        CONSOLE.print (f'Node {r1.name} {r1.status}',style='success')
+        
+        
+        # link_r1_DUT.get()
+        # link_r1_DUT.delete()
+
+    def get_links_node(self,node_name):
+        lab = Project(name=self.name_lab , connector=self.connector )
+        lab.get()
+        r1 = Node(
+            project_id=lab.project_id, 
+            name=node_name,
+            connector=self.connector
+            ) # создаем экз-р устр-ва
+        r1.get()
+        link_r1_sw3 = r1.links[1].nodes # получаем 1й линк r1_sw3
+        # Извлекаем из словаря имена линков списком
+        texts = [item['label']['text'] for item in link_r1_sw3]
+        for text in texts:
+            print(texts)  #  Получим свой лдинк и линк на другой стороне
+
+        
+      
+
+    
 if __name__=="__main__":
     name_lab = 'SSV_auto_Tr_GRE'
     gns = ConnectGNS(name_lab = 'SSV_auto_Tr_GRE')
-    
-    # print (gns.get_ver_gns(),'\n')
-    # print(gns.all_proj(),'\n')
+    node_name = 'R1'
     name_lab = 'SSV_auto_Tr_GRE'
-    print(gns.get_nodes(),'\n')
 
-    # print(gns.start_node())
-    # print(gns.start_nodes_from_project())
-    # print( gns.get_nodes(),'\n')
+    print(gns.get_links_node(node_name),'\n')
