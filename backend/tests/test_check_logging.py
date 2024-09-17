eacaf9858c499fa4f8e38b3ba11cadebdceacbea
SergeyVolkoff <vol7745@yandex.ru>
2024-09-17 17:01:27 +0300

    add test upgrade fw, add pipeliane stage upgrade_check_fw

--- backend/tests/test_check_logging.py
+++ backend/tests/test_check_logging.py
@@ -1,6 +1,4 @@
 """ Тесты проверки поддержки логгирования."""
-import pytest
-import yaml
 import sys
 import os
 import allure
@@ -11,5 +9,5 @@ from checks.check_logging import *
 @allure.feature ('Тесты логгирования событий.')
 @allure.story('2.проверка логгирования событий в файл.')
 def test_check_logging_file():
-     assert check_logging_file()==True, f'Логиррование не ведется или ведется не в файл logs!'
+     assert check_logging_file()==True, f'Логиррование не ведется или ведется не в файл logs!'  # noqa: E712, F405, F541
 
