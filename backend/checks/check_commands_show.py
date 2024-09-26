e34bf7115ad7807156b422c4b7d03ec8cf972cb4
SergeyVolkoff <vol7745@yandex.ru>
2024-09-26 10:22:07 +0300

    fix pipe

--- backend/checks/check_commands_show.py
+++ backend/checks/check_commands_show.py
@@ -105,53 +105,7 @@ def check_logging_file():
         # br100.ssh.disconnect()
         return False
 
-def check_int_link_speed(interface):
-        output_cli = br100.ssh.send_command_timing(f'show interface eth0',read_timeout=1)
-        try:
-            if 'link-speed' in output_cli:
-                regex_output = re.search(r'link-speed (?P<link_speed>.+)',output_cli)
-                link_speed = regex_output.group('link_speed')
-            else:
-                return False
-        except AttributeError as err:
-            print(err, 
-                    f"Вызвано исключение при отправке комады:reg_output.group() на вывод cli:{output_cli} "
-                    )
-            return False
-        print('link_speed -', link_speed)
-        return True
-    
-def check_int_mtu(interface):
-    output_cli = br100.ssh.send_command_timing(f'show interface {interface}')
-    try:
-        if 'mtu' in output_cli:
-            regex_output = re.search(r'mtu (?P<mtu_size>\d+)',output_cli)
-            mtu_size = regex_output.group('mtu_size')
-        else:
-            return False
-    except AttributeError as err:
-        print(err, 
-                f"Вызвано исключение при отправке комады:reg_output.group() на вывод cli:{output_cli} "
-                )
-        return False
-    print('mtu_size =', mtu_size)
-    return True
 
-def check_int_duplex(interface):
-    output_cli = br100.ssh.send_command_timing(f'show interface {interface}')
-    try:
-        if 'duplex' in output_cli:
-            regex_output = re.search(r'duplex(?P<duplex>\S+)',output_cli)
-            duplex = regex_output.group('duplex')
-        else:
-            return False
-    except AttributeError as err:
-        print(err, 
-                f"Вызвано исключение при отправке комады:reg_output.group() на вывод cli:{output_cli} "
-                )
-        return False
-    print('duplex',duplex)
-    return True
 
 if __name__ == "__main__":
     print(check_int_mtu('eth0'))
